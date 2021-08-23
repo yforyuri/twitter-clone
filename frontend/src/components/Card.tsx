e7905fcca29d90f32f2bda570a4d255033b1c0b6
yforyuri <yforyurirho@gmail.com>
2021-08-23 10:43:09 +0900

    set layout

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,19 +1,52 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
 import React, { FC } from 'react';
 import { ITweet } from '../interfaces';
 import ProfileIcon from './ProfileIcon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faComment as farComment,
+  faHeart as farHeart,
+} from '@fortawesome/free-regular-svg-icons';
+import { faRetweet, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
 
 interface CardsProps {
   tweet: ITweet;
 }
 
 const Card: FC<CardsProps> = ({ tweet }) => {
+  dayjs.extend(relativeTime);
   return (
-    <div className="flex">
-      <div>
+    <li className="flex border-b-1">
+      <div className="mt-4 mx-4">
         <ProfileIcon />
       </div>
-      <div>{tweet.users.nickname}</div>
-    </div>
+      <div className="mt-6 text-sm bg-red-100 w-full ml-4">
+        <span className="font-bold">{tweet.users.nickname}</span>
+        <span className="ml-2 text-gray-500">
+          {dayjs(tweet.createdAt).locale('ko').fromNow()}
+        </span>
+        <div>{tweet.tweet}</div>
+        <div className="flex justify-between my-4">
+          <div className="w-full">
+            <FontAwesomeIcon icon={farComment} />
+            <span className="ml-2">123</span>
+          </div>
+          <div className="w-full">
+            <FontAwesomeIcon icon={faRetweet} />
+            <span className="ml-2">123</span>
+          </div>
+          <div className="w-full">
+            <FontAwesomeIcon icon={farHeart} />
+            <span className="ml-2">123</span>
+          </div>
+          <div className="w-full">
+            <FontAwesomeIcon icon={faEllipsisH} />
+          </div>
+        </div>
+      </div>
+    </li>
   );
 };
 
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,9 +2,11 @@ import React, { FC } from 'react';
 
 const Layout: FC = ({ children }) => {
   return (
-    <div className="flex bg-red-200 min-h-screen font-noto">
+    <div className="flex min-h-screen font-noto">
       <div className="flex-auto">1</div>
-      <div className="max-w-screen-sm flex-auto">{children}</div>
+      <div className="max-w-screen-sm flex-auto border-r-1 border-l-1">
+        {children}
+      </div>
       <div className="flex-auto">3</div>
     </div>
   );
