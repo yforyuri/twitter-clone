c62a340935e5924f6ee3bfe7fa29a4f9d256ace6
yforyuri <yforyurirho@gmail.com>
2021-09-07 16:31:24 +0900

    aws config setting

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -11,11 +11,14 @@ import { v4 as uuid } from 'uuid';
 import { extname } from 'path';
 import * as multerS3 from 'multer-s3';
 import * as AWS from 'aws-sdk';
+import { config } from 'dotenv';
+
+config();
 
 AWS.config.update({
-  accessKeyId: `${process.env.AWS_ACCESS_KEY_ID}`,
-  secretAccessKey: `${process.env.AWS_SECRET_ACCESS_KEY}`,
-  region: `${process.env.AWS_REGION}`,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
 });
 
 const s3 = new AWS.S3();
